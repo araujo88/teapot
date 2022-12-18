04b4e411533ff7775b274423c32528e0e993a684
Leonardo Araujo <leonardo.aa88@gmail.com>
2022-12-18 02:27:37 -0300

    example model

--- include/model.hpp
+++ include/model.hpp
@@ -1,4 +1,20 @@
 #ifndef MODEL_H_
 #define MODEL_H_
 
+#include <string>
+
+class Person
+{
+private:
+    std::string name;
+    std::string surname;
+
+    Person();
+    std::string getName();
+    std::string getSurname();
+    void setName(std::string name);
+    void setSurname(std::string surname);
+    ~Person();
+};
+
 #endif
