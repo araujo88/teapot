9853b203516ae31620ecb769b7c78ab0728f4745
Leonardo Araujo <leonardo.aa88@gmail.com>
2024-03-22 02:09:43 -0300

    feat: updated model

--- include/model.hpp
+++ include/model.hpp
@@ -8,12 +8,14 @@ namespace tpt
     class Model
     {
     private:
+        unsigned int id;
         std::string name;
         std::string surname;
 
     public:
         Model();
         Model(std::string name, std::string surname);
+        unsigned int getId();
         std::string getName();
         std::string getSurname();
         void setName(std::string name);
--- src/model.cpp
+++ src/model.cpp
@@ -4,6 +4,34 @@ using namespace tpt;
 
 Model::Model()
 {
+    this->name = "John";
+    this->surname = "Doe";
+}
+
+Model::Model(std::string name, std::string surname)
+{
+    this->name = name;
+    this->surname = surname;
+}
+
+std::string Model::getName()
+{
+    return this->name;
+}
+
+std::string Model::getSurname()
+{
+    return this->surname;
+}
+
+void Model::setName(std::string name)
+{
+    this->name = name;
+}
+
+void Model::setSurname(std::string surname)
+{
+    this->surname = surname;
 }
 
 Model::~Model()
